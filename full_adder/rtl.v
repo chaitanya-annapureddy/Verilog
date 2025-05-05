ea6ef7e6406b084a8dcb6315498bdd841d375852
Chaitanya annapureddy <chaitanyaannapureddy206@gmail.com>
2025-05-05 21:21:54 +0530

    Update rtl.v

--- full_adder/rtl.v
+++ full_adder/rtl.v
@@ -3,14 +3,16 @@ module full_adder ( input a,b,c,
                    output sum,carry);
 
   wire w1,w2,w3;
-  assign w1 = a ^ b;
-  assign w2 = a & b;
-  assign w3  = c & w1;
+  //half_adder instantiation  by name based
+  half_adder ha1 (.a(a),.b(b),.sum(w1),.carry(w2));
+  half_adder ha2 (.a(w1),.b(c),.sum(sum),.carry(w3));
 
-  assign sum = ( w1 ^ c);
-  assign carry = (w2 | w1);
+  //or gate
+  assign carry = (w2 | w3 );
 
 endmodule
+
+
                   
 
 
