80dedd70eb3180866fb578f36e1b3bebdbb625e2
Chaitanya annapureddy <chaitanyaannapureddy206@gmail.com>
2025-05-06 15:55:44 +0530

    Create tb.v

---
+++ half_subtractor/tb.v
@@ -0,0 +1,18 @@
+module half_sub_tb.v ;
+  reg a,b;
+  wire diff,borrow;
+
+  half_subtractor dut ( a,b,diff,borrow );
+
+  initial 
+    begin
+       a = 0 ; b = 0 ; #2;
+       a = 0 ; b = 1 ; #2;
+       a = 1; b =  0 ; #2;
+       a = 1 ; b = 1; #2; 
+      %display (
+        $monitor("time=%0t,a= %b , b = %b ,diff = %b,borrow = %b, $time,a,b,diff,borrow);
+      #100  $finish;
+               
+      
+    end
