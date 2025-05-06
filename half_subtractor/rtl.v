eb6276321091600f4120f8e45d598ef7f3689ae5
Chaitanya annapureddy <chaitanyaannapureddy206@gmail.com>
2025-05-06 15:42:06 +0530

    Update rtl.v

--- half_subtractor/rtl.v
+++ half_subtractor/rtl.v
@@ -1,1 +1,8 @@
+`timescale 1ns / 1ps 
+module half_subtractor ( input a,b,
+                        output diff,borrow);
+  assign diff = (a ^ b);
+  assign borrow  = (~a & b);
 
+endmodule
+                    
