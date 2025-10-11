7f26e8e4823cfb2d28b10f9233a6194d11f74f1e
Chaitanya annapureddy <chaitanyaannapureddy206@gmail.com>
2025-10-11 17:46:44 +0530

    Update up_dwn_cnt_tb.v

--- counter/up_dwn_cnt_tb.v
+++ counter/up_dwn_cnt_tb.v
@@ -1,1 +1,53 @@
 
+
+	module tb;
+		
+	 reg [2:0] t =0;
+	 reg clk = 0,rst =0,mode = 1;
+
+	 wire [2:0]count;
+	 
+ 	 //dut instantiation 
+
+	 up_down duv (t,clk,rst,mode,count);
+
+	 always #5 clk = ~clk;
+
+	 task rest();
+	   begin
+		  @(negedge clk)
+		    rst = 1;
+		  @(negedge clk)
+		    rst = 0;
+	   end
+	 endtask
+
+	 task values(input a,input b);
+	   begin
+		   @(negedge clk)
+		   t = a;
+		   mode = b;
+	   end
+	 endtask
+	
+	 initial
+	   begin
+		  rest();
+		  values(111,1);
+		  values(111,0);
+		  
+	   end 
+	 
+	initial
+	    begin
+	       $monitor($time,"count is %d",count);
+       		
+	   	#200;
+		$finish;
+	    end
+endmodule
+
+
+    
+
+
